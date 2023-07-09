a919e72e6d377a674f8913ad06a86adaa9b2bfcb
Omer Ozer <omerozer5505@gmail.com>
2023-07-09 13:17:13 +0300

    added all functionality of the ERC6551

--- README.md
+++ README.md
@@ -3,7 +3,17 @@ Basic Scripts and Contracts to work with ERC6551
 Try running some of the following tasks:
 
 ```shell
-npx hardhat run scripts/Registy_Create_account.js  => to run the account creation
+npx hardhat run scripts/Registy_Create_account.js  => to run the account creation and other functionalities of the register and account
 npx hardhat verify --network goerli <address> => to verify the contracts in the etherscan
-npx hardhat run scripts/deployAccount.js --network goerli => to deploy any contract to goerli
+npx hardhat run scripts/deployAccount.js --network goerli => to deploy Account to goerli
+npx hardhat run scripts/deployRegistry.js --network goerli => to deploy Registry to goerli
+```
+
+Example .env:
+
+```shell
+PRIVATE_KEY=<your private key>
+GOERLI_RPC_URL=https://eth-goerli.g.alchemy.com/v2/<your alchemy apı key>
+ETHERSCAN_API_KEY=<your etherscan apı key>
+INFURA_KEY = <your infura apı key>
 ```
--- scripts/Registy_Create_account.js
+++ scripts/Registy_Create_account.js
@@ -88,6 +88,76 @@ const ERC6551RegistryABI = [
     },
 ];
 
+const ERC6551AccountABI = [
+    {
+        inputs: [
+            { internalType: "uint256", name: "_size", type: "uint256" },
+            { internalType: "uint256", name: "_start", type: "uint256" },
+            { internalType: "uint256", name: "_end", type: "uint256" },
+        ],
+        name: "InvalidCodeAtRange",
+        type: "error",
+    },
+    {
+        inputs: [
+            { internalType: "address", name: "to", type: "address" },
+            { internalType: "uint256", name: "value", type: "uint256" },
+            { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        name: "executeCall",
+        outputs: [{ internalType: "bytes", name: "result", type: "bytes" }],
+        stateMutability: "payable",
+        type: "function",
+    },
+    {
+        inputs: [
+            { internalType: "bytes32", name: "hash", type: "bytes32" },
+            { internalType: "bytes", name: "signature", type: "bytes" },
+        ],
+        name: "isValidSignature",
+        outputs: [
+            { internalType: "bytes4", name: "magicValue", type: "bytes4" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "nonce",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "owner",
+        outputs: [{ internalType: "address", name: "", type: "address" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [
+            { internalType: "bytes4", name: "interfaceId", type: "bytes4" },
+        ],
+        name: "supportsInterface",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "pure",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "token",
+        outputs: [
+            { internalType: "uint256", name: "chainId", type: "uint256" },
+            { internalType: "address", name: "tokenContract", type: "address" },
+            { internalType: "uint256", name: "tokenId", type: "uint256" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    { stateMutability: "payable", type: "receive" },
+];
+
 const RegistryAddress = "0x2018a38f1c8e486a99c0729e6cf8337f5401ceb9";
 
 async function main() {
@@ -103,18 +173,64 @@ async function main() {
     );
 
     // Now you can call functions on the Registry contract
-    const tx = await Registry.createAccount(
-        "0x3C3AFA9F2aaf933B2048626cC1278f31155F42D7", // Address of the account created (ERC6551 Account contract)
+    // Creating ERC6551 Account from registry.
+    /*const tx = await Registry.createAccount(
+        "0xA601bEF0d7f417AB74074Ef0E2fB205669243f50", // Address of the account created (ERC6551 Account contract)
         5, //  chainId
         "0xFc3287b7508a0783665fbCA5C8847628475c83e9", // parent NFT
         9, // token ID
         0, // salt
         "0x" // init calldata
+    );*/
+
+    // Getting Account address
+    const NewAccountAddress = await Registry.account(
+        "0xA601bEF0d7f417AB74074Ef0E2fB205669243f50", // Address of the account created (ERC6551 Account contract)
+        5, //  chainId
+        "0xFc3287b7508a0783665fbCA5C8847628475c83e9", // parent NFT
+        9, // token ID
+        0 // salt
     );
 
-    const receipt = await tx.wait();
+    console.log("Account Created at:", NewAccountAddress);
 
-    console.log(receipt);
+    const recipientAddress = NewAccountAddress;
+    const amountToSend = ethers.parseEther("0.02");
+
+    const transaction = {
+        to: recipientAddress,
+        value: amountToSend,
+    };
+
+    await wallet
+        .sendTransaction(transaction)
+        .then((tx) => {
+            console.log("Transaction sent");
+        })
+        .catch((error) => {
+            console.error("Failed to send transaction:", error);
+        });
+
+    const Account_contract = new ethers.Contract(
+        NewAccountAddress,
+        ERC6551AccountABI,
+        wallet
+    );
+
+    const owner = await Account_contract.owner();
+    console.log(owner);
+
+    // Balance can't be updated instantly. Returns the old value
+    //const balance = await provider.getBalance(NewAccountAddress);
+    //console.log("Balance of the Account: ", ethers.formatEther(balance));
+
+    const sendETHfromAccount = await Account_contract.executeCall(
+        "0xEC6c574E296e5553F7C59f01e122Abc0340f0D4E",
+        "10000000000000000",
+        "0x"
+    );
+
+    console.log(sendETHfromAccount);
 }
 
 main().catch((error) => {
